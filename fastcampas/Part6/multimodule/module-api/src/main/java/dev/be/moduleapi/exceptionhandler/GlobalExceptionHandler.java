2cf5a50e3d76ce021b4ced4e69fb937a11594140
netager <thinkjbank@gmail.com>
2024-01-13 17:22:46 +0900

    20240112 Review Spring Multimodule

---
+++ fastcampas/Part6/multimodule/module-api/src/main/java/dev/be/moduleapi/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package dev.be.moduleapi.exceptionhandler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
---
+++ fastcampas/Part6/multimodule/module-api/src/main/java/dev/be/moduleapi/response/CommonResponse.java
@@ -0,0 +1,34 @@
+package dev.be.moduleapi.response;
+
+import dev.be.modulecommon.enums.CodeEnum;
+import lombok.*;
+
+
+@Getter
+@Setter
+@Builder
+@JsonInclude(ToString.Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResponse<T> {
+    private String returnCode;
+    private String returnMessage;
+    private T info;
+
+    public CommonResponse(CodeEnum codeEnum) {
+        setReturnCode(codeEnum.getCode());
+        setReturnMessage(codeEnum.getMessage());
+    }
+
+    public CommonResponse(T info) {
+        setReturnCode(CodeEnum.SUCCESS.getCode());
+        setReturnMessage(CodeEnum.SUCCESS.getMessage());
+        setInfo(info);
+    }
+
+    public CommonResponse(CodeEnum codeEnum, T info) {
+        setReturnCode(codeEnum.getCode());
+        setReturnMessage(codeEnum.getMessage());
+        setInfo(info);
+    }
+}
